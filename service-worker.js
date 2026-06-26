39da125e20678b82243e7c2674098d0707982877
guleb68 <guillaume.leblanc.1@gmail.com>
2026-06-25 23:07:48 -0400

    Add files via upload
    Remplacer Icone iPhone

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "horaire-usi-partage-v88";
-const APP_SHELL = ["./", "./index.html", "./styles.css?v=84", "./assets/logo-medi-cal.png", "./vendor-xlsx.full.min.js", "./api-client.js?v=4", "./app.js?v=84", "./pwa.js?v=1", "./manifest.json?v=86", "./icons/medi-cal-v2-180.png", "./icons/medi-cal-v2-192.png", "./icons/medi-cal-v2-512.png", "./icons/medi-cal-v2-maskable-192.png", "./icons/medi-cal-v2-maskable-512.png"];
+const CACHE_NAME = "horaire-usi-partage-v89";
+const APP_SHELL = ["./", "./index.html", "./styles.css?v=84", "./assets/logo-medi-cal.png", "./vendor-xlsx.full.min.js", "./api-client.js?v=4", "./app.js?v=84", "./pwa.js?v=1", "./manifest.json?v=87", "./icons/medi-cal-v3-180.png", "./icons/medi-cal-v3-192.png", "./icons/medi-cal-v3-512.png", "./icons/medi-cal-v3-maskable-192.png", "./icons/medi-cal-v3-maskable-512.png"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
